be0dafd0e47e09dc6fc5b56ccee8f418cac1086f
Prasanna Annamalai <prasanna.annamalai@gmail.com>
2015-02-20 16:15:22 +0530

    Initial Commit

---
+++ src/main/scala/Parser.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by prannamalai on 2/20/15.
+ */
+package com.paypal.genio
+
+class Parser
---
+++ src/test/scala/ParserSpec.scala
@@ -0,0 +1,19 @@
+/**
+ * Created by prannamalai on 2/20/15.
+ */
+
+package com.paypal.genio.spec
+
+import org.scalatest.FunSpec
+
+class ParserSpec extends FunSpec {
+
+  // TODO: Remove the default passing test case
+
+
+  describe("Adding 1 to 1") {
+    it("should equals 2"){
+      assert(1+1 == 2)
+    }
+  }
+}
